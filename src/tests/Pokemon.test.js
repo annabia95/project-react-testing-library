46f735c73c4883a8f8b060f5026416bb6a740ca2
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-03-22 20:23:42 -0300

    Cumprindo o sexto requisito

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Sexto Requisito', () => {
+  it('É renderizado um card com as informações de determinado pokémon', () => {
+    renderWithRouter(<App />);
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName).toHaveTextContent('Pikachu');
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType).toHaveTextContent('Electric');
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    expect(pokemonWeight).toHaveTextContent('Average weight: 6.0 kg');
+    const img = screen.getByAltText(/sprite/i);
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+  it('O card contém um link de navegação para exibir detalhes', () => {
+    const { history } = renderWithRouter(<App />);
+    const maisDetalhes = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(maisDetalhes);
+    expect(history.location.pathname).toBe('/pokemons/25');
+  });
+  it('Existe um ícone de estrela nos Pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+    const maisDetalhes = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(maisDetalhes);
+    const checkbox = screen.getByRole('checkbox');
+    userEvent.click(checkbox);
+    const img = screen.getByAltText(/is marked as favorite/i);
+    expect(img).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
