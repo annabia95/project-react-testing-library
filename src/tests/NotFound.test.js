a12ed8bc440da4295bfecd74b882e50d003b3be9
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-03-22 17:37:35 -0300

    Cumpindo o quarto requisito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Terceiro Requisito', () => {
+  it('A página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const title = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(title).toBeInTheDocument();
+  });
+  it('A página página mostra a imagem ', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img', { name: /Pikachu crying because the page/i });
+    expect(img).toBeInTheDocument();
+    expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
