26349e599383cd10f28a1bee3890bb59683ed479
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-03-22 16:15:12 -0300

    Cumpindo o primeiro requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+/* import { Router, MemoryRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history'; */
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Primeiro Requisito', () => {
+  it('O topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    const linkFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+    expect(linkHome).toBeDefined();
+    expect(linkAbout).toBeDefined();
+    expect(linkFavorite).toBeDefined();
+  });
+  it('A aplicação é redirecionada para a página inicial ao clicar no link Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    userEvent.click(linkHome);
+    expect(history.location.pathname).toBe('/');
+  });
+  it('A aplicação é redirecionada para a página sobre ao clicar no link About', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    userEvent.click(linkAbout);
+    expect(history.location.pathname).toBe('/about');
+  });
+  it('A aplicação é redirecionada para a página inicial ao clicar no link Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(linkFavorite);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+});
---
+++ src/tests/renderWithRouter.js
@@ -0,0 +1,18 @@
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+import React from 'react';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return {
+    ...render(
+      <Router history={ history }>
+        {component}
+      </Router>,
+    ),
+    history,
+  };
+};
+
+export default renderWithRouter;
